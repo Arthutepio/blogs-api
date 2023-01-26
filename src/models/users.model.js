cb80aba6e7484aff92656ece242a28839cb72a92
arthutepio <arthurodriguesr10@gmail.com>
2023-01-26 18:26:34 -0300

    feat: cria tabela users atraves do users.model

---
+++ src/models/users.model.js
@@ -0,0 +1,18 @@
+const UsersSchema = (sequelize, DataTypes) => {
+  const UsersTable = sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+    {
+      tableName: 'users',
+      underscored: true,
+      timestamps: false,
+    },
+  );
+
+  return UsersTable;
+};
+
+module.exports = UsersSchema;
